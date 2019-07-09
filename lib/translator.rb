12dec60f3f531510b83927d29994fd218fb4c3dc
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-09 20:52:49 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,17 +1,18 @@
 # require modules here
+require 'yaml'
 
-def load_library(file_path)
+def load_library
   # code goes here
   
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon
   # code goes here
   
 end
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning
   # code goes here
-  emo_library = load_library(file_path)
+ 
   
 end
