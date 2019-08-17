befa10216b5f22e6b5282dfbb357a35a39c2846e
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-17 19:14:50 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,14 @@ require "pry"
 def load_library(filepath)
   library = {}
   emoticons = YAML.load_file(filepath)
-  library = {get_meaning: emoticons.keys, get_emoticon: emoticons.values}
+  library = {get_meaning: {}, get_emoticon: {}}
+  emoticons.each_pair do |key, value|
+    #binding.pry
+    library[:get_meaning][value[1]] = key
+    library[:get_emoticon][value[0]] = value[1]
+  end
   library
+  #binding.pry
 end
 
 def get_japanese_emoticon
